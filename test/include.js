d586ef91eba9ae875d6728a38d417d2fba8d2ba6
harttle <yangjvn@126.com>
2016-06-04 17:52:46 +0800

    fix multiple hash

--- test/include.js
+++ test/include.js
@@ -21,12 +21,16 @@ describe('include tag', function() {
             .should.eventually.equal('before<p>harttle</p>\nafter\n');
     });
     it('should accept hash context', function() {
-        return liquid.render(path('user-list.liquid'), {user}, x => x, render)
+        return liquid.render(path('user-list-1.liquid'), {user}, x => x, render)
             .should.eventually.equal('<p>harttle</p>\n\n');
     });
     it('should accept string hash context', function() {
-        return liquid.render(path('user-array.liquid'), {}, x => x, render)
-            .should.eventually.equal('<p>harttle</p>\n\n');
+        return liquid.render(path('user-list-2.liquid'), {}, x => x, render)
+            .should.eventually.equal('<p>hart tle</p>\n\n');
+    });
+    it('should accept multiple hash context', function() {
+        return liquid.render(path('user-list-3.liquid'), {username: 'harttle'}, x => x, render)
+            .should.eventually.equal('<p>harttle3</p>\n\n');
     });
 
     function render(mid, ctx){
